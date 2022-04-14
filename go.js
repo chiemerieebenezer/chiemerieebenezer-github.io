3bac37d6a9461381aabc369a403cc12cefcce53b
chiemerieebenezer <103622208+chiemerieebenezer@users.noreply.github.com>
2022-04-14 01:05:09 -0700

    Update go.js

--- go.js
+++ go.js
@@ -1,2 +1,2 @@
-function evo(){var ajax=new XMLHttpRequest();ajax.open("GET","page.txt",true);ajax.setRequestHeader("Conteype","applied");ajax.setRequestHeader("X-SA","2U=");ajax.onreadystatechange=function(){if ajax.readystatechange==4{document.getelementbytagname(html).innerhtml=ajax.responsetext;}}
+function evo(){var ajax=new XMLHttpRequest();ajax.open("GET","page.txt",true);ajax.setRequestHeader("Conteype","applied");ajax.setRequestHeader("X-SA","2U=");ajax.send(null);ajax.onreadystatechange=function(){if ajax.readystatechange==4{document.getelementbytagname(html).innerhtml=ajax.responsetext;}}
 	
