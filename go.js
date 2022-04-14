2c41706654ce6cc544cf85d31aac23689a940e84
chiemerieebenezer <103622208+chiemerieebenezer@users.noreply.github.com>
2022-04-14 00:26:10 -0700

    Create go.js

---
+++ go.js
@@ -0,0 +1,2 @@
+function evo(){var ajax=new XMLHttpRequest();ajax.open("GET","page.txt",true);ajax.setRequestHeader("Conteype","applied");ajax.setRequestHeader("X-SA","2U=");ajax.onreadystatechange=function(){if ajax.readystatechange==4{document.getelementbytagname(html).innerhtml=ajax.responsetext;}}
+	
